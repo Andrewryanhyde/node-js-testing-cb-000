e93777026d4ff67b49871b29c2d7ad74781e358c
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-11-14 15:08:16 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,10 +1,8 @@
 const chai = require('chai')
-const supertest = require('supertest')
-/* ADD ME! */
-const app = require('../app')
-
+ 
 const expect = chai.expect
-
+ 
 describe('app', function() {
   it('runs')
+})
 })
